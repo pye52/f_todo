f3f897486b5e38933875e2db9ba593a7101083ba
pye52 <pye52@163.com>
2020-11-03 23:47:54 +0800

    开发中

--- lib/widget/todo_list.dart
+++ lib/widget/todo_list.dart
@@ -10,11 +10,6 @@ class TodoList extends StatefulWidget {
 
 class TodoListState extends State<TodoList> {
   @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return FutureBuilder<List<Todo>>(
       future: Future(() async {
@@ -32,42 +27,67 @@ class TodoListState extends State<TodoList> {
         Log.debug("待办事项列表数量: ${list.length}");
         return ListView.separated(
           padding: const EdgeInsets.symmetric(vertical: 8),
-          itemBuilder: (context, index) => _buildItem(list[index]),
+          itemBuilder: (context, index) => TodoItem(list[index]),
           separatorBuilder: (context, index) => SizedBox(height: 8),
           itemCount: list.length,
         );
       },
     );
   }
+}
 
-  _buildItem(Todo item) => Card(
-        margin: const EdgeInsets.symmetric(horizontal: 16),
-        child: Container(
-          height: 50,
-          alignment: Alignment.centerLeft,
-          color: item.completed ? Colors.black26 : Colors.white,
-          padding: const EdgeInsets.all(8),
-          child: Row(
-            children: [
-              Checkbox(
-                  value: item.completed,
-                  onChanged: (value) async {
-                    Log.debug(
-                        "待办事项id: ${item.id}, '${item.content}', 状态变更: ${value}");
-                    item.completed = value;
-                    await item.save();
-                    setStateSafely(() {});
-                  }),
-              Text(
-                item.content,
-                style: TextStyle(
-                  decoration: item.completed
-                      ? TextDecoration.lineThrough
-                      : TextDecoration.none,
-                ),
-              ),
-            ],
+class TodoItem extends StatefulWidget {
+  final Todo item;
+  TodoItem(this.item);
+  @override
+  State createState() => TodoItemState();
+}
+
+class TodoItemState extends State<TodoItem> {
+  @override
+  Widget build(BuildContext context) {
+    var item = widget.item;
+    return Container(
+      height: 50,
+      alignment: Alignment.centerLeft,
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: TextButton(
+        style: ButtonStyle(
+          foregroundColor: MaterialStateProperty.all(
+            item.completed ? Colors.grey : Colors.black,
+          ),
+          backgroundColor: MaterialStateProperty.all(
+            item.completed ? Colors.black26 : Colors.white,
           ),
         ),
-      );
+        onPressed: () {
+          _onItemClicked(item, completed: !item.completed);
+        },
+        child: Row(
+          children: [
+            Checkbox(
+                value: item.completed,
+                onChanged: (value) async {
+                  _onItemClicked(item, completed: value);
+                }),
+            Text(
+              item.content,
+              style: TextStyle(
+                decoration: item.completed
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onItemClicked(item, {completed = true}) async {
+    Log.debug("待办事项id: ${item.id}, '${item.content}', 状态变更: $completed");
+    item.completed = completed;
+    await item.save();
+    setStateSafely(() {});
+  }
 }
