97d33b3bf1dca0e61a1ed64b8926344ca1db11b0
pye52 <pye52@163.com>
2020-12-10 20:48:14 +0800

    开发中...

--- lib/module/login/msc_login.dart
+++ lib/module/login/msc_login.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'dart:math';
 
 import 'package:crypto/crypto.dart';
+import 'package:dio/dio.dart';
 import 'package:dio_log/dio_log.dart';
 import 'package:f_todo/api/microsoft_dio.dart';
 import 'package:f_todo/log/global_logger.dart';
@@ -15,39 +16,47 @@ class MscLoginPage extends StatefulWidget {
   State createState() => MscLoginPageState();
 }
 
-const String client_id = "181ad653-2fdd-41e3-9463-3e3fd8569ada";
-const String msc_redirect_uri = "https://www.kanade.ftodo.com";
-const String msc_scope = "Calendars.ReadWrite";
-const String msc_login_url =
-    '''https://login.microsoftonline.com/common/oauth2/v2.0/authorize?
-client_id=$client_id
+const String _grant = "common";
+const String _clientId = "181ad653-2fdd-41e3-9463-3e3fd8569ada";
+const String _mscRedirectUri = "https://www.kanade.ftodo.com";
+const String _mscScope = "Calendars.ReadWrite";
+const String _mscLoginUrl =
+    '''https://login.microsoftonline.com/$_grant/oauth2/v2.0/authorize?
+client_id=$_clientId
 &response_type=code
-&redirect_uri=$msc_redirect_uri
+&redirect_uri=$_mscRedirectUri
 &response_mode=query
-&scope=$msc_scope''';
-const String msc_token_url =
-    "https://login.microsoftonline.com/commo/oauth2/v2.0/token";
+&scope=$_mscScope
+&code_challenge_method=S256''';
+const String _mscTokenUrl =
+    "https://login.microsoftonline.com/$_grant/oauth2/v2.0/token";
 
 class MscLoginPageState extends State<MscLoginPage> {
-  List<int> _codeVerifier = List.empty(growable: true);
   WebViewController _controller;
+  String _codeVerifier;
   String _codeChallenge;
 
   @override
   void initState() {
     super.initState();
+    _codeVerifier = "";
     var random = new Random();
     // 生成PKCE用到的code_verifier
     for (var i = 0; i < 48; i++) {
-      _codeVerifier.add(random.nextInt(10));
+      _codeVerifier += random.nextInt(10).toString();
     }
-    var sha = sha256.convert(_codeVerifier);
-    _codeChallenge = base64.encode(sha.bytes);
-    var lastIndex = _codeChallenge.length - 1;
-    // 必须去掉编码后最后一个'='
-    if (_codeChallenge[lastIndex] == '=') {
-      _codeChallenge = _codeChallenge.substring(0, lastIndex);
-    }
+    _codeVerifier = "ThisIsntRandomButItNeedsToBe43CharactersLong";
+    List<int> bytes = utf8.encode(_codeVerifier);
+    var sha = sha256.convert(bytes).toString();
+    var shaBytes = utf8.encode(sha.toString());
+    Log.debug("sha: $sha, string: ${sha.toString()}");
+    _codeChallenge = base64.encode(shaBytes);
+    // var lastIndex = _codeChallenge.length - 1;
+    // // 必须去掉编码后最后一个'='
+    // if (_codeChallenge[lastIndex] == '=') {
+    //   _codeChallenge = _codeChallenge.substring(0, lastIndex);
+    // }
+    Log.debug("code verifier: $_codeVerifier, code challenge: $_codeChallenge");
   }
 
   @override
@@ -61,11 +70,10 @@ class MscLoginPageState extends State<MscLoginPage> {
         onWebViewCreated: (controller) {
           _controller = controller;
         },
-        initialUrl:
-            "$msc_login_url&code_challenge=$_codeChallenge&code_challenge_method=S256",
+        initialUrl: "$_mscLoginUrl&code_challenge=$_codeChallenge",
         javascriptMode: JavascriptMode.unrestricted,
         onPageStarted: (url) {
-          if (!url.startsWith(msc_redirect_uri)) {
+          if (!url.startsWith(_mscRedirectUri)) {
             return;
           }
           // 登录成功，截取code，示例返回：https://www.kanade.ftodo.com/#code=M.R3_BAY.b91e965b-8cfb-5ad3-9ca2-b05868e20c72
@@ -84,25 +92,29 @@ class MscLoginPageState extends State<MscLoginPage> {
             return;
           }
           var code = successMatcher.group(1);
-          Log.debug("login code: $code");
+          Log.debug("login code: $code, code verifier: $_codeVerifier");
           _controller.loadUrl("about:blank");
           // 请求access_token
-          scheduleMicrotask(getAccessToken);
+          scheduleMicrotask(() => getAccessToken(code));
         },
       ),
     );
   }
 
-  void getAccessToken() async {
+  void getAccessToken(String code) async {
     var response = await MicrosoftService.dio().post(
-      msc_token_url,
+      _mscTokenUrl,
       data: {
-        "client_id": client_id,
-        "scope": msc_scope,
-        "redirect_uri": msc_redirect_uri,
         "grant_type": "authorization_code",
+        "client_id": _clientId,
+        "scope": _mscScope,
+        "code": code,
+        "redirect_uri": _mscRedirectUri,
         "code_verifier": _codeVerifier,
       },
+      options: Options(
+        contentType: "application/x-www-form-urlencoded",
+      ),
     );
     Log.debug("response: $response");
   }
